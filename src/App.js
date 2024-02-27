232ec9dccbac39eee09b76c7cf4d990afe142521
Wellington Bezerra <wellingtonbezerra.30512@gmail.com>
2024-02-26 22:53:59 -0300

    Criando conexão com a API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from './services/api';
 
 function App() {
+  let dados;
+  useEffect(() => {
+    api.get().then(res => console.log(res))
+  },[])
+
   return (
-    <>Teste</>
+    <>Inicio</>
+    
   );
 }
 
